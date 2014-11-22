f50eba9a6a0c5cd23e5c09a68708ffd8c919b501
Jacob Shay <jake_ps@comcast.net>
2014-11-21 22:05:01 -0800

    test

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141025173729) do
+ActiveRecord::Schema.define(version: 20141121052110) do
 
   create_table "users", force: true do |t|
     t.string   "email"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20141025173729) do
     t.string   "auth_token"
     t.string   "password_reset_token"
     t.datetime "password_reset_sent_at"
+    t.string   "username"
   end
 
 end
